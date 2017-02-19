7c18a4e2187346df4c15f655d9860e66e9edb5c3
Kuba Mikita <jakub@underdev.it>
2017-02-19 17:45:23 +0100

    initial version 1

---
+++ inc/triggers.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Triggers class
+ */
+
+namespace Notification\bbPress;
+
+use Notification\Singleton;
+use Notification\bbPress\Triggers;
+
+class Triggers extends Singleton {
+
+	public function __construct() {
+
+		$this->load_triggers();
+
+		add_filter( 'notification/disable/post_types_allowed', array( $this, 'disable_metabox_post_types' ), 10, 1 );
+
+	}
+
+	/**
+	 * Load triggers from their directories
+	 * @return void
+	 */
+	public function load_triggers() {
+
+		// Forums
+		new Triggers\Forums();
+
+		// Topics
+		new Triggers\Topics();
+
+		// Replies
+		new Triggers\Replies();
+
+	}
+
+	/**
+	 * Allow bbPress post types to disable certain triggers
+	 * @param  array $post_types default post types
+	 * @return array             post types with bbPress types
+	 */
+	public function disable_metabox_post_types( $post_types ) {
+
+		$post_types[] = bbp_get_forum_post_type();
+		$post_types[] = bbp_get_topic_post_type();
+		$post_types[] = bbp_get_reply_post_type();
+
+		return $post_types;
+
+	}
+
+}
